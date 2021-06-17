8d076c46fd83296fd954e569272f134e65580a1a
10405 <10405@GSOURCE123>
2021-06-17 18:26:07 +0530

    MainMenu -Create a Menu BluePrint -Initialisers and Ubutton Callbacks -Changig UI input modes

---
+++ Source/PuzzlePlatforms/MenuSystem/MainMenu.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainMenu.h"
+#include "Components/Button.h"
+
+bool UMainMenu::Initialize() {
+
+	bool Success = Super::Initialize();
+	if (!Success) return false;
+
+
+	if (!ensure(Host != nullptr)) return false;
+	Host->OnClicked.AddDynamic(this, &UMainMenu::HostServer);
+
+	return true;
+
+}
+
+void UMainMenu::SetMenuInterface(IMenuInterface* InMenuInterface)
+{
+	MenuInterface = InMenuInterface;
+}
+
+
+void UMainMenu::HostServer()
+{
+	UE_LOG(LogTemp, Warning, TEXT("I'm gonna host a server!"));
+
+	if (MenuInterface != nullptr)
+	{
+		MenuInterface->Host();
+	}
+}
---
+++ Source/PuzzlePlatforms/MenuSystem/MainMenu.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuInterface.h"
+#include "MainMenu.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetMenuInterface(IMenuInterface* InMenuInterface);
+
+protected:
+	virtual bool Initialize();
+
+
+private:
+		UPROPERTY(meta = (BindWidget))
+		class UButton* Host;
+
+	UPROPERTY(meta = (BindWidget))
+		class UButton* Join;
+
+	UFUNCTION()
+		 void HostServer();
+
+	     IMenuInterface* MenuInterface;
+
+
+};
